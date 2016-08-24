825fed791b86f65574b9cbadfde8e396fbf66d7d
Ioannis Charalampidis <johnys2@gmail.com>
2016-08-24 14:19:46 +0200

    Some bug fixes

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
 
 var Generator = require('./lib/generator.js');
 
-if (process.argv.length < 2) {
+if (process.argv.length < 4) {
 	console.error('Usage: node generate.js [file.raml] [type]');
 	process.exit(1);
 } else {
-	console.log(JSON.stringify(data, null, '  '));
+	var generator = new Generator(process.argv[2]);
+	console.log(JSON.stringify(generator.generate(process.argv[3]), null, '  '));
 }
